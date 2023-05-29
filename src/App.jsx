197c75decf29f3aab8c885e0c3b93864e51391c3
Wagner Pedro <wagnerpedro355@gmail.com>
2023-05-29 04:26:53 -0700

    buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import './App.css'
+import Calculator from './components/Calculator'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <Calculator />
     </>
   )
 }
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,27 @@
+import './Calculator.module.css'
+
+export default function Calculator() {
+  return (
+    <div className="wrapper">
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>0</button>
+      <button>,</button>
+      <button>ac</button>
+      <button>+/-</button>
+      <button>%</button>
+      <button>/</button>
+      <button>X</button>
+      <button>-</button>
+      <button>+</button>
+      <button>=</button>
+    </div>
+  );
+}
